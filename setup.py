b28935ffb6a97c7c54c083c330288897f8d97d3e
LE POTIER Alexandre <a-le-potier@arafer.fr>
2022-06-22 16:59:56 +0200

    add names parameter in Csv.to_df

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="datamanipy",
-    version="1.2.1",
+    version="1.2.2",
     author="Alexandre Le Potier",
     description="A Python package that provides tools to help you manipulating data.",
     classifiers=[
